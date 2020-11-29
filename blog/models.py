7e993913b11b31da7c2928d6bee8e12471cb85b7
Валентина <valentina@MacBook-Pro-Valentina.local>
2020-11-29 14:04:38 +0300

    Add Postgres

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+	blog_title = models.CharField(max_length = 100)
+	blog_date = models.DateField()
+	blog_text = models.CharField(max_length = 300)
+	blog_image = models.ImageField(upload_to = 'mblog_images/')
